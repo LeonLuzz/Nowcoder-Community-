39ea13c052c485209091153e020ca93df8e69419
leon <longjianjiang@qq.com>
2022-05-18 19:46:50 +0800

    首次提交

--- src/test/java/com/nowcoder/community/MailTest.java
+++ src/test/java/com/nowcoder/community/MailTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.nowcoder.community.util.MailClient;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @SpringBootTest
 public class MailTest {
@@ -11,9 +13,22 @@ public class MailTest {
     @Autowired
     private MailClient mailClient;
 
+    @Autowired
+    private TemplateEngine templateEngine;
+
     @Test
     public void testTextMain() {
         mailClient.sendMail("2869241810@qq.com", "Test", "Leon has coming!");
     }
 
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "Hello world!");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("2869241810@qq.com", "HTML", content);
+    }
 }
