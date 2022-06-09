f56977fdd4ff10a498ac324d1108258c99c2e0bf
leon <longjianjiang@qq.com>
2022-06-09 14:57:24 +0800

    显示用户收到的赞！

--- src/main/java/com/nowcoder/community/aspect/ServiceLogAspect.java
+++ src/main/java/com/nowcoder/community/aspect/ServiceLogAspect.java
@@ -6,9 +6,7 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -4,6 +4,7 @@ public class RedisKeyUtil {
 
     private static final String SPILT = ":";
     private static final String PREFIX_ENTITY_LIKE = "like:entity";
+    private static final String PREFIX_USER_LIKE = "like:user";
 
     // 某个实体的赞
     // like:entity:entityType:entityId -> set(userId)
@@ -11,4 +12,9 @@ public class RedisKeyUtil {
         return PREFIX_ENTITY_LIKE + SPILT + entityType + SPILT + entityId;
     }
 
+    // 某个用户的赞
+    // like:user:userId -> int
+    public static String getUserLikeKey(int userId) {
+        return PREFIX_USER_LIKE + SPILT + userId;
+    }
 }
