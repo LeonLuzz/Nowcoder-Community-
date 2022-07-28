e18bdabb4edab12a669c0866d49d1288d48ec7b8
longjianjiang <leonluz@foxmial.com>
2022-07-28 10:19:00 +0800

    Elasticsearch测试用例通过

--- src/test/java/com/nowcoder/community/ElasticsearchTest.java
+++ src/test/java/com/nowcoder/community/ElasticsearchTest.java
@@ -15,6 +15,8 @@ import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.elasticsearch.search.sort.SortBuilders;
 import org.elasticsearch.search.sort.SortOrder;
 import org.junit.jupiter.api.Test;
@@ -132,7 +134,58 @@ public class ElasticsearchTest {
             System.out.println(post);
         }
     }
-    // 别人写的 大概看了下文档，没问题直接用了
+
+    // 高亮显示
+    @Test
+    public void highlightQuery() throws Exception{
+        SearchRequest searchRequest = new SearchRequest("discusspost");//discusspost是索引名，就是表名
+        Map<String,Object> res = new HashMap<>();
+
+        //高亮
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.field("title");
+        highlightBuilder.field("content");
+        highlightBuilder.requireFieldMatch(false);
+        highlightBuilder.preTags("<span style='color:red'>");
+        highlightBuilder.postTags("</span>");
+
+        //构建搜索条件
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder()
+                .query(QueryBuilders.multiMatchQuery("互联网寒冬", "title", "content"))
+                .sort(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+                .sort(SortBuilders.fieldSort("score").order(SortOrder.DESC))
+                .sort(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+                .from(0)// 指定从哪条开始查询
+                .size(10)// 需要查出的总记录条数
+                .highlighter(highlightBuilder);//高亮
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        List<DiscussPost> list = new ArrayList<>();
+        long total = searchResponse.getHits().getTotalHits().value;
+        for (SearchHit hit : searchResponse.getHits().getHits()) {
+            DiscussPost discussPost = JSONObject.parseObject(hit.getSourceAsString(), DiscussPost.class);
+
+            // 处理高亮显示的结果
+            HighlightField titleField = hit.getHighlightFields().get("title");
+            if (titleField != null) {
+                discussPost.setTitle(titleField.getFragments()[0].toString());
+            }
+            HighlightField contentField = hit.getHighlightFields().get("content");
+            if (contentField != null) {
+                discussPost.setContent(contentField.getFragments()[0].toString());
+            }
+            // System.out.println(discussPost);
+            list.add(discussPost);
+        }
+        res.put("list",list);
+        res.put("total",total);
+        if(res.get("list")!= null){
+            for (DiscussPost post : list = (List<DiscussPost>) res.get("list")) {
+                System.out.println(post);
+            }
+            System.out.println(res.get("total"));
+        }
+    }
 }
 
 
